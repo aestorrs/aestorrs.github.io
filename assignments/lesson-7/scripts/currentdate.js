fc6be6e99f4617f0945d07bc1b6014ec96a4f682
aestorrs <sto17042@byui.edu>
2018-02-24 23:09:09 -0700

    Catch up Sync

---
+++ assignments/lesson-7/scripts/currentdate.js
@@ -0,0 +1,4 @@
+var months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+var tomorrow = new Date();
+tomorrow.setTime(tomorrow.getTime() + (1000 * 3600 * 24));
+document.getElementById("spanDate").innerHTML = tomorrow.getDay() + ", " + tomorrow.getDate() + ", " + months[tomorrow.getMonth()] + " " + tomorrow.getFullYear();
