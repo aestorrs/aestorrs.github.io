9f21a7e1f08d28faf48046eb47d2bf98c8e2f455
aestorrs <sto17042@byui.edu>
2018-02-28 22:57:30 -0700

    Week 7 changes and Week 8 Table

--- assignments/lesson-6/scripts/currentdate.js
+++ assignments/lesson-6/scripts/currentdate.js
@@ -1,4 +1,8 @@
-var months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
-var tomorrow = new Date();
-tomorrow.setTime(tomorrow.getTime() + (1000 * 3600 * 24));
-document.getElementById("spanDate").innerHTML = tomorrow.getDay() + ", " + tomorrow.getDate() + ", " + months[tomorrow.getMonth()] + " " + tomorrow.getFullYear();
+document.addEventListener('DOMContentLoaded', function(){ 
+// Wait for the page to be ready
+	var months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+	var tomorrow = new Date();
+	console.log(tomorrow + 'is tomorrow');
+	tomorrow.setTime(tomorrow.getTime() + (1000 * 3600 * 24));
+	document.getElementById("spanDate").innerHTML = tomorrow.getDay() + ", " + tomorrow.getDate() + ", " + months[tomorrow.getMonth()] + " " + tomorrow.getFullYear();
+}, false);
