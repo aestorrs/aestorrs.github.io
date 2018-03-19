1ae107e1a09780822e4ee3e07544b8bb78318c48
aestorrs <sto17042@byui.edu>
2018-03-19 00:29:34 -0600

    Franklin Changes

--- assignments/weathersite/scripts/currentdate.js
+++ assignments/weathersite/scripts/currentdate.js
@@ -1,4 +1,5 @@
+.document.addEventListener('DOMContentLoaded', function(){
 var months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
 var tomorrow = new Date();
 tomorrow.setTime(tomorrow.getTime() + (1000 * 3600 * 24));
-document.getElementById("spanDate").innerHTML = tomorrow.getDay() + ", " + tomorrow.getDate() + ", " + months[tomorrow.getMonth()] + " " + tomorrow.getFullYear();
+document.getElementById("spanDate").innerHTML = tomorrow.getDay() + ", " + tomorrow.getDate() + ", " + months[tomorrow.getMonth()] + " " + tomorrow.getFullYear();}, false);
--- assignments/weathersite/scripts/hamburger.js
+++ assignments/weathersite/scripts/hamburger.js
@@ -1,3 +1,18 @@
+document.addEventListener('DOMContentLoaded', function(){
+	document.getElementsByClassName("navigation")[0].classList.toggle("responsive");
+
+}, false);
+
+var visibleSmallMenu = false;
 function toggleHam() {
-    document.getElementsByClassName("navigation")[0].classList.toggle("responsive");
+	console.log('clicked hamburger button');	
+	var ulNav = document.getElementsByClassName("navigation")[0];
+		if(visibleSmallMenu) {
+			ulNav.style.display = 'none';
+			visibleSmallMenu = false;
+		} else {
+				ulNav.style.display = 'block';
+				visibleSmallMenu = true;
+		}
+   
 }
